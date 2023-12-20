3dcb8c1adc59cfac4b1463c6cf892de002a1a6b8
Mayur <kmayur819@gmail.com>
2023-12-20 21:38:36 +0530

    made some change now the screen is visible

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:projectchess/screens/intial.dart';
 
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,10 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: theme,
       home: IntialScreen(
         title: 'chess-v',
       ),
--- lib/screens/intial.dart
+++ lib/screens/intial.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import "package:flutter/material.dart";
 
 class IntialScreen extends StatelessWidget {
@@ -5,15 +7,35 @@ class IntialScreen extends StatelessWidget {
     super.key,
     required String title,
   });
-  String title = "Chess-V";
+  String title = 'Chess-V';
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chess-V'),
+        title: Text(title),
       ),
-      body: Row(),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Container(
+            width: width / 2, // Set a width
+            height: height / 4, // Set a height
+            color: Colors.red,
+          ),
+          Container(
+            width: width / 2,
+            height: height / 4,
+            color: Colors.blue,
+          ),
+          Container(
+            width: width / 2,
+            height: height / 4,
+            color: Colors.green,
+          ),
+        ]),
+      ),
     );
   }
 }
